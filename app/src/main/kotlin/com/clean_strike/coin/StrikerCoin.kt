f1c7aa06efbbb1705cca66b0a61cb35b0de05123
Harsh Bhandari <harshb@sahaj.ai>
2023-02-26 22:29:55 +0530

    Add a striker coin

---
+++ app/src/main/kotlin/com/clean_strike/coin/StrikerCoin.kt
@@ -0,0 +1,8 @@
+package com.clean_strike.coin
+
+class StrikerCoin : Coin {
+    override fun getPoints(): Int {
+        return 0
+    }
+
+}
---
+++ app/src/test/kotlin/com/clean_strike/coin/StrikerCoinTest.kt
@@ -0,0 +1,13 @@
+package com.clean_strike.coin
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class StrikerCoinTest {
+    @Test
+    fun `Should check the points of striker coin to be 0`() {
+        val strikerCoin = StrikerCoin()
+
+        assertEquals(0, strikerCoin.getPoints())
+    }
+}
