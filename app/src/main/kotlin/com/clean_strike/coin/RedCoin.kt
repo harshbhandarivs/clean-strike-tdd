66045df0474591b86d61a13059844d82cba83503
Harsh Bhandari <harshb@sahaj.ai>
2023-02-26 22:29:23 +0530

    Update tests for red coin

--- app/src/main/kotlin/com/clean_strike/coin/RedCoin.kt
+++ app/src/main/kotlin/com/clean_strike/coin/RedCoin.kt
@@ -1,7 +1,9 @@
 package com.clean_strike.coin
 
-class RedCoin {
+class RedCoin: Coin {
     companion object {
         const val POINTS = 3
     }
+
+    override fun getPoints(): Int = POINTS
 }
--- app/src/test/kotlin/com/clean_strike/coin/RedCoinTest.kt
+++ app/src/test/kotlin/com/clean_strike/coin/RedCoinTest.kt
@@ -6,6 +6,8 @@ import kotlin.test.assertEquals
 class RedCoinTest {
     @Test
     fun `Should check the points of black coin to be 1`() {
-        assertEquals(3, RedCoin.POINTS)
+        val redCoin = RedCoin()
+
+        assertEquals(3, redCoin.getPoints())
     }
 }
